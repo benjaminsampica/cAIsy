ae4a42fb99d0e75055fde23cffcc79fbc760ac86
Christian Snook <css6143@corp.caseys.com>
2023-06-16 12:28:53 -0500

    Chat History Changes

--- src/Caisy.Web/Features/Home/Home.razor.cs
+++ src/Caisy.Web/Features/Home/Home.razor.cs
@@ -1,12 +1,15 @@
 ﻿using Caisy.Web.Features.Profile;
+using Microsoft.JSInterop;
 using OpenAI_API;
 using OpenAI_API.Chat;
+using System.Text.Json;
 
 namespace Caisy.Web.Features.Home;
 
 public partial class Home
 {
     [Inject] public ISnackbar Snackbar { get; set; } = null!;
+    [Inject] public IJSRuntime JSRuntime { get; set; } = null!;
     [CascadingParameter] public ProfileState ProfileState { get; set; } = null!;
     private OpenAIAPI OpenAiApi { get; set; }
     private OpenApiRequest _request = new();
@@ -31,13 +34,29 @@ public partial class Home
     private async Task OnSourceChangedAsync(string value)
     {
         if (_source == value) return;
-        _source = value;
+
+        if (_conversation.Messages.Count > 1)
+        {
+            var chatDetail = new ChatDetail(_conversation.Messages.ToList(), _source, _destination);
+            var chatHistory = JsonSerializer.Serialize(chatDetail);
+            await JSRuntime.InvokeVoidAsync("localStorage.setItem", DateTime.Now.ToString(), chatHistory);
+        }
+
+        _source = value;        
         _conversation = OpenAiApi.Chat.CreateConversation();
         _conversation.AppendSystemMessage($"Convert {_source} to {_destination}");
     }
     private async Task OnDestinationChangedAsync(string value)
     {
         if (_destination == value) return;
+
+        if (_conversation.Messages.Count > 1)
+        {
+            var chatDetail = new ChatDetail(_conversation.Messages.ToList(), _source, _destination);
+            var chatHistory = JsonSerializer.Serialize(chatDetail);
+            await JSRuntime.InvokeVoidAsync("localStorage.setItem", DateTime.Now.ToString(), chatHistory);
+        }
+
         _destination = value;
         _conversation = OpenAiApi.Chat.CreateConversation();
         _conversation.AppendSystemMessage($"Convert {_source} to {_destination}");

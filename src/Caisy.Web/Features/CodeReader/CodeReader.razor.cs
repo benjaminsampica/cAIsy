9c9e6bc5b5a0dee63b4874bbe3376d6c72772f5e
Andrew Davidson <678836@corp.caseys.com>
2023-06-16 11:12:34 -0500

    add code reader

---
+++ src/Caisy.Web/Features/CodeReader/CodeReader.razor.cs
@@ -0,0 +1,61 @@
+﻿using Caisy.Web.Features.Profile;
+using Microsoft.AspNetCore.Components.Forms;
+using OpenAI_API;
+using OpenAI_API.Chat;
+
+namespace Caisy.Web.Features.CodeReader;
+
+public partial class CodeReader
+{
+    [Inject] public ISnackbar Snackbar { get; set; } = null!;
+    [CascadingParameter] public ProfileState ProfileState { get; set; } = null!;
+    private OpenAIAPI OpenAiApi { get; set; }
+    private OpenApiRequest _request = new();
+    private Conversation _conversation;
+    private bool _isInProgress = false;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (ProfileState.ApiKey != null)
+        {
+            OpenAiApi = new OpenAIAPI(ProfileState.ApiKey);
+        }
+
+        _conversation = OpenAiApi.Chat.CreateConversation();
+        _conversation.AppendSystemMessage("Document the code provided. Explain in simple terms.");
+    }
+
+    private async Task OnValidSubmitAsync()
+    {
+        _isInProgress = true;
+
+        _conversation.AppendUserInput(_request.Prompt);
+
+        await _conversation.GetResponseFromChatbotAsync();
+
+        _isInProgress = false;
+    }
+
+    private async Task OnFileUploadAsync(InputFileChangeEventArgs e)
+    {
+        if (e.FileCount == 0) return;
+
+        var file = e.File;
+        using (var streamReader = new StreamReader(file.OpenReadStream()))
+        {
+            var fileContent = await streamReader.ReadToEndAsync();
+            _request.Prompt = fileContent;
+        }
+    }
+}
+
+public class OpenApiRequest
+{
+    public string? Prompt { get; set; }
+    public string? TestCaseFramework { get; set; }
+}
+
+public class OpenApiResponse
+{
+    public string? Response { get; set; }
+}

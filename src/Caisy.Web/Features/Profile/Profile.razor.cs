fb45bfbc9d94489af75d11ce265cae8b6c49c94d
Ben Sampica <684314@corp.caseys.com>
2023-06-16 09:27:28 -0500

    Update theming.

--- src/Caisy.Web/Features/Profile/Profile.razor.cs
+++ src/Caisy.Web/Features/Profile/Profile.razor.cs
@@ -4,16 +4,19 @@ namespace Caisy.Web.Features.Profile;
 
 public partial class Profile : IDisposable
 {
-    [Inject] public ProfileState ProfileState { get; set; } = null!;
     [Inject] public IRepository<UserProfile> ProfileRepository { get; set; } = null!;
     [Inject] public ISnackbar Snackbar { get; set; } = null!;
+    [CascadingParameter] public ProfileState ProfileState { get; set; } = null!;
 
     private readonly CancellationTokenSource _cts = new();
     private readonly ProfileRequest _model = new();
 
+    private string GetThemeIcon() => _model.PrefersDarkMode ? Icons.Material.Filled.DarkMode : Icons.Material.Filled.LightMode;
+
     protected override async Task OnInitializedAsync()
     {
         _model.ApiKey = ProfileState?.ApiKey ?? string.Empty;
+        _model.PrefersDarkMode = ProfileState?.PrefersDarkMode ?? false;
     }
 
     private async Task OnValidSubmitAsync()
@@ -23,11 +26,14 @@ public partial class Profile : IDisposable
             await ProfileRepository.RemoveAsync(ProfileState.Id, _cts.Token);
         }
 
-        var newUserProfile = new UserProfile { ApiKey = _model.ApiKey };
+        var newUserProfile = new UserProfile { ApiKey = _model.ApiKey, PrefersDarkMode = _model.PrefersDarkMode };
         await ProfileRepository.AddAsync(newUserProfile, _cts.Token);
 
-        ProfileState.ApiKey = newUserProfile.ApiKey;
         ProfileState.Id = newUserProfile.Id;
+        ProfileState.ApiKey = newUserProfile.ApiKey;
+        ProfileState.PrefersDarkMode = newUserProfile.PrefersDarkMode;
+
+        ProfileState.OnUserSettingsChanged?.Invoke();
 
         Snackbar.Add("Successfully updated profile.", Severity.Success);
     }
@@ -42,10 +48,15 @@ public partial class Profile : IDisposable
 public class ProfileRequest
 {
     [Required] public string ApiKey { get; set; } = null!;
+    public bool PrefersDarkMode { get; set; }
 }
 
 public class ProfileState
 {
     public string? ApiKey { get; set; }
     public string? Id { get; set; }
+    public bool PrefersDarkMode { get; set; }
+    public CodeBlockTheme CodeBlockTheme => PrefersDarkMode ? CodeBlockTheme.AtomOneDark : CodeBlockTheme.AtomOneLight;
+    public Action? OnUserSettingsChanged { get; set; }
+    public MudThemeProvider MudThemeProvider { get; set; } = null!;
 }
